0eb90451b66f2b386c89aeb7d4e4377ffb66aca5
jp-sena-dev <joaopedrosena1997@gmail.com>
2024-05-29 18:07:03 -0300

    :sparkles: create: hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="banner">
+    <div class="apresentation">
+      <img src="../assets/images/logo.svg" alt="logo cooking" class="logo">
+
+      <p class="header-lg header-phrase">
+        A feast of flavors to awaken 
+        <span class="green-text">the chef within you!</span>
+      </p>
+
+      <p class="caption-lg">
+        Embark on a culinary adventure daily, with ingredients within arm's reach, unveiling new recipes!
+      </p>
+    </div>
+
+    <img src="../assets/images/banner.png" alt="foto de uma mulher cozinhando com uma baci de virdro" class="image-banner">
+  </header>
+</template>
+
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--medium-gray);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.header-phrase {
+  margin-bottom: 2rem;
+}
+
+.green-text {
+  color: var(--medium-green, #3D6D4A);
+}
+
+.image-banner {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .image-banner {
+    width: min(100%, 21.25rem);
+  }
+}
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{ text: string; active?: boolean; }>();
+</script>
+
+<template>
+  <span :class="['tag', { active }]">
+    {{ text }}
+  </span>
+</template>
+
+<style scoped>
+.tag {
+  display: inline-block;
+  border-radius: 0.5rem;
+  min-width: 4.25rem;
+  padding: 0.5rem;
+  text-align: center;
+  transition: 0.2s;
+  color: var(--gray);
+  background-color: var(--light-gray);
+  font-weight: 400;
+}
+
+.tag.active {
+  color: var(--white);
+  background: var(--coral);
+  font-weight: 700;
+}
+</style>
