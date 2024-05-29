35f8c6cb9d34fec1734637958c62171e7c7a3dcb
jp-sena-dev <joaopedrosena1997@gmail.com>
2024-05-29 18:09:24 -0300

    :sparkles: create: button to select ingredients

---
+++ src/components/SelectableIngredient.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import Tag from './Tag.vue';
+
+  const props = defineProps<{ ingredient: string }>();
+  const emit = defineEmits(['addIngredient', 'removeIngredient']);
+
+  const selected = ref(false);
+
+  const handleClick = () => {
+    selected.value = !selected.value
+
+    selected.value ? emit('addIngredient', props.ingredient) : emit('removeIngredient', props.ingredient);
+  }
+
+</script>
+
+<template>
+  <button class="ingredient" @click="handleClick()" :aria-pressed="selected">
+    <Tag :text="ingredient" :active="selected" />
+  </button>
+</template>
+
+
+<style scoped>
+  .ingredient {
+    cursor: pointer;
+  }
+</style>
