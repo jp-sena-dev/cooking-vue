209e6ffd3fc9cc61f9833711b60d2e26de463a73
jp-sena-dev <joaopedrosena1997@gmail.com>
2024-05-29 18:05:21 -0300

    :tada: start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HeroSection from './components/HeroSection.vue'
+import MainContainer from './components/MainContainer.vue';
+</script>
+
+<template>
+  <HeroSection />
+  <MainContainer />
+</template>
+
+
+<style scoped>
+
+</style>
+
